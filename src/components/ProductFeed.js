a8894a0a7c84265ece39c23aaa74f1eccea62ac4
emmanuel <emmanueltaiwo027@gmail.com>
2023-02-19 18:11:29 +0100

    Fixed tailwind error

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,7 @@ import Product  from './Product'
 
 const ProductFeed = ({ products }) => {
   return (
-    <div className='grid grid-flow-row-dense  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-32 mx-auto'>
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-32 mx-auto'>
       
       {products.slice(0, 4).map(({ id, title, price, description, category, image }) => (
         <Product
@@ -19,7 +19,7 @@ const ProductFeed = ({ products }) => {
 
       <img className='md:col-span-full' src="https://links.papareact.com/dyz " alt="" />
 
-      <div className='md:col-span-5'>
+      <div className='md:col-span-2'>
       {products.slice(4, 5).map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
@@ -33,8 +33,7 @@ const ProductFeed = ({ products }) => {
       ))}
       </div>
 
-      
-      {products.slice(5, 12).map(({ id, title, price, description, category, image }) => (
+      {products.slice(5, 11).map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
           id={id}
@@ -46,8 +45,9 @@ const ProductFeed = ({ products }) => {
         />
       ))}
 
+
 <img className='md:col-span-full' src="https://links.papareact.com/dyz " alt="" />
-{products.slice(12, 16).map(({ id, title, price, description, category, image }) => (
+{products.slice(11, 15).map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
           id={id}
